01a952e51f2405f044cb178a393a9a54b9c9a458
mycoding5555 <mycoding5555@gmail.com>
2025-08-19 21:51:00 +0700

    #done for fetch User

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,7 @@ INSERT INTO users (name, email) VALUES ('Alice', 'alice@example.com');
 INSERT INTO users (name, email) VALUES ('Bob', 'bob@example.com');
 INSERT INTO attendances (user_id, check_in_time, check_out_time) VALUES (1, '2025-08-18T08:00:00', '2025-08-18T17:00:00');
 INSERT INTO attendances (user_id, check_in_time, check_out_time) VALUES (2, '2025-08-18T09:00:00', '2025-08-18T18:00:00');
+
+ALTER TABLE users ADD COLUMN password VARCHAR(255);
+
+INSERT INTO users (name, email, password) VALUES ('User', 'charlie@example.com', 'User123');
